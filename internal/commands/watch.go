c033ef42c18b94938488b5f69aa4bdb0c12b0bb1
kamichidu <c.kamunagi@gmail.com>
2024-10-23 11:38:34 +0900

    netclip watch --stream

--- internal/commands/watch.go
+++ internal/commands/watch.go
@@ -2,14 +2,21 @@ package commands
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
+	"github.com/kamichidu/go-netclip/clipboard"
 	"github.com/kamichidu/go-netclip/internal/metadata"
 	"github.com/urfave/cli/v2"
 )
 
 var cmdWatch = &cli.Command{
-	Name:   "watch",
+	Name: "watch",
+	Flags: []cli.Flag{
+		&cli.BoolFlag{
+			Name: "stream",
+		},
+	},
 	Action: doWatch,
 }
 
@@ -18,12 +25,26 @@ func init() {
 }
 
 func doWatch(c *cli.Context) error {
+	stream := c.Bool("stream")
 	store := metadata.GetStore(c.App.Metadata)
+
+	var write func(clipboard.Event) error
+	if stream {
+		je := json.NewEncoder(c.App.Writer)
+		write = func(evt clipboard.Event) error {
+			return je.Encode(evt)
+		}
+	} else {
+		write = func(evt clipboard.Event) error {
+			_, err := fmt.Fprintf(c.App.Writer, "%v\n", evt)
+			return err
+		}
+	}
 
 	ctx := context.Background()
 	ch := store.Watch(ctx)
 	for evt := range ch {
-		fmt.Fprintf(c.App.Writer, "%v\n", evt)
+		write(evt)
 	}
 	return nil
 }
